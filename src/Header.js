791595e465e918e42a12dca2deeacc81ba6d7c80
Wilson Susanto s3737493 <wilsonzhaang666@gmail.com>
2021-07-28 00:15:03 +1000

    Tried to added header and waiting the permission of using boostrap

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+
+function Header() {
+  return (
+      <div>
+<div className="header">
+  <a href="#default" class="logo">CompanyLogo</a>
+  <div className="header-right">
+    <a className="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
+
+<div >
+  <h1>Responsive Header</h1>
+  <p>Resize the browser window to see the effect.</p>
+  <p>Some content..</p>
+</div>
+</div>
+  );
+}
+
+export default Header;
